30e64d61aa2f2640cc97baa7dbea0c21f79d6808
Alberto Barradas <abcsds@gmail.com>
2015-11-08 01:33:52 -0600

    Update general

--- data/convert.py
+++ data/convert.py
@@ -9,14 +9,10 @@ with open('sdal.json', 'w') as outfile:
 
     for line in lines:
         word, pleasure, activation, imagination, p_sdev, a_sdev, i_sdev = line.split(";")
-        pleasure = str(float(pleasure) - 2)
-        activation = str(float(activation) - 2)
-        imagination = str(float(imagination) - 2)
+        pleasure = str(round((float(pleasure) - 2),4))
+        activation = str(round((float(activation) - 2),4))
+        imagination = str(round((float(imagination) - 2),4))
         word, obj = word.split("_")
-        word = str(word.rstrip())+":\n    "+str({"obj":obj.rstrip(), "pleasure": pleasure.rstrip(), "activation": activation.rstrip(),"imagination": imagination.rstrip(),"p_sdev": p_sdev.rstrip(),"a_sdev": a_sdev.rstrip(),"i_sdev": i_sdev.rstrip() })+",\n"
+        word = str(word.rstrip())+":"+str({"obj":obj.rstrip(), "pleasure": pleasure.rstrip(), "activation": activation.rstrip(),"imagination": imagination.rstrip(),"p_sdev": p_sdev.rstrip(),"a_sdev": a_sdev.rstrip(),"i_sdev": i_sdev.rstrip() })+",\n"
         outfile.writelines(word)
-
-    # json.dump(data, outfile)
     outfile.writelines("\n}")
-# and write everything back
-#
