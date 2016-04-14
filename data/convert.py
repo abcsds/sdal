440d98bc9d286d2f4391b40aa80f11d99069a789
Alberto Barradas <abcsds@gmail.com>
2016-04-13 23:49:31 -0500

    Update json format - Correct to double quotes - Convert.py uses json.dumps

--- data/convert.py
+++ data/convert.py
@@ -1,6 +1,6 @@
 import json
 
-with open('orig.csv', 'r') as infile:
+with open('sdal.csv', 'r') as infile:
     lines = infile.readlines()
 
 with open('sdal.json', 'w') as outfile:
@@ -8,11 +8,11 @@ with open('sdal.json', 'w') as outfile:
     outfile.writelines("{\n")
 
     for line in lines:
-        word, pleasure, activation, imagination, p_sdev, a_sdev, i_sdev = line.split(";")
-        pleasure = str(round((float(pleasure) - 2),4))
+        word, pleasantness, activation, imagination, p_sdev, a_sdev, i_sdev = line.split(";")
+        pleasantness = str(round((float(pleasantness) - 2),4))
         activation = str(round((float(activation) - 2),4))
         imagination = str(round((float(imagination) - 2),4))
         word, obj = word.split("_")
-        word = str(word.rstrip())+":"+str({"obj":obj.rstrip(), "pleasure": pleasure.rstrip(), "activation": activation.rstrip(),"imagination": imagination.rstrip(),"p_sdev": p_sdev.rstrip(),"a_sdev": a_sdev.rstrip(),"i_sdev": i_sdev.rstrip() })+",\n"
+        word = '"'+str(word.rstrip())+'"'+":"+json.dumps({"obj":obj.rstrip(), "pleasantness": pleasantness.rstrip(), "activation": activation.rstrip(),"imagination": imagination.rstrip(),"p_sdev": p_sdev.rstrip(),"a_sdev": a_sdev.rstrip(),"i_sdev": i_sdev.rstrip() })+",\n"
         outfile.writelines(word)
     outfile.writelines("\n}")
